849c5eb977022cd9fe4ef261463dcea108c5f01a
Justin Zhou <justinzhou68@gmail.com>
2021-08-02 14:48:45 +1200

    Added and styled the nav bar

---
+++ landing-page-burger/src/components/NavBar/NavBarData.js
@@ -0,0 +1,40 @@
+
+
+export const links = [
+  {
+    id: 1,
+    url: '/',
+    text: 'Home',
+  },
+  {
+    id: 2,
+    url: '/Menu',
+    text: 'Menu',
+  },
+  {
+    id: 3,
+    url: '/Deals',
+    text: 'Deals',
+  },
+  {
+    id: 4,
+    url: '/Bundles',
+    text: 'Bundles',
+  },
+  {
+    id: 5,
+    url: '/About Us',
+    text: 'About Us',
+  },
+  {
+    id: 6,
+    url: '/Stores',
+    text: 'Stores',
+  },
+  {
+    id: 7,
+    text: 'Order Now',
+    url: '/Order Now',
+  },
+];
+
